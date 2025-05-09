2bc9132dc12775bbb5dcc1a6a0201a6c3d1ff793
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-09 20:24:02 -0300

    refactor(app): register conceitos modules in AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConceitosManualModule } from './conceitos-manual/conceitos-manual.module';
+import { ConceitosAutomaticoModule } from './conceitos-automatico/conceitos-automatico.module';
 
 @Module({
-  imports: [],
+  imports: [ConceitosManualModule, ConceitosAutomaticoModule],
   controllers: [AppController],
   providers: [AppService],
 })
