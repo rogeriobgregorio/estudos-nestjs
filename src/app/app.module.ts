4ffb239f759b7a59dba588bc35acd7696246a3c9
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-10 22:49:37 -0300

    refactor(app): restructure core files into 'app' directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConceitosManualModule } from './conceitos-manual/conceitos-manual.module';
-import { ConceitosAutomaticoModule } from './conceitos-automatico/conceitos-automatico.module';
+import { ConceitosManualModule } from '../conceitos-manual/conceitos-manual.module';
+import { ConceitosAutomaticoModule } from '../conceitos-automatico/conceitos-automatico.module';
 
 @Module({
   imports: [ConceitosManualModule, ConceitosAutomaticoModule],
