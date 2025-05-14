a89608235113123f912a81943834073455d4f724
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-13 21:42:04 -0300

    refactor: update recados controller and module to use new service

--- crud-nest/src/recados/recados.controller.ts
+++ crud-nest/src/recados/recados.controller.ts
@@ -1,35 +1,45 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
+import { RecadosService } from './recados.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 
 @Controller('recados')
 export class RecadosController {
+  constructor(private readonly RecadosService: RecadosService) {}
 
-    @HttpCode(HttpStatus.OK)
-    @Get()
-    findAll(@Query() pagination: any) {
-        const { limit, offset } = pagination;
-        return `This action returns all recados with ${limit} limit and ${offset} offset`;
-    }
+  @HttpCode(HttpStatus.OK)
+  @Get()
+  findAll(@Query() pagination: { limit: number; offset: number }) {
+    const { limit, offset } = pagination;
+    return this.RecadosService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return `This action returns a #${id} recado`;
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.RecadosService.findOne(+id);
+  }
 
-    @Post()
-    create(@Body() body: any) {
-        return body;
-    }
+  @Post()
+  create(@Body() body: any) {
+    return this.RecadosService.create(body);
+  }
 
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() body: any) {
-        return {
-            id,
-            ...body,
-        };
-    }
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body: any) {
+    return this.RecadosService.update(+id, body);
+  }
 
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return `This action removes a #${id} recado`;
-    }
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.RecadosService.remove(+id);
+  }
 }
