d6afd8b023c7450017e597c52ad15cfef193c090
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-15 19:25:43 -0300

    refactor: update create recado DTO

--- crud-nest/src/recados/dto/create-recado.dto.ts
+++ crud-nest/src/recados/dto/create-recado.dto.ts
@@ -1,5 +1,21 @@
+import { IsNotEmpty, IsString, MinLength, MaxLength } from 'class-validator';
+
 export class CreateRecadoDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(255)
   readonly texto: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(250)
   readonly de: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(5)
+  @MaxLength(250)
   readonly para: string;
 }
