15c411befb5a8bbd5abf3df6867ed2091254af12
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-15 20:30:49 -0300

    refactor: update recados controller logic

--- crud-nest/src/recados/recados.controller.ts
+++ crud-nest/src/recados/recados.controller.ts
@@ -9,6 +9,7 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  ParseIntPipe,
   Patch,
   Post,
   Query,
@@ -26,8 +27,8 @@ export class RecadosController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.RecadosService.findOne(+id);
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.RecadosService.findOne(id);
   }
 
   @Post()
@@ -36,12 +37,15 @@ export class RecadosController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRecadoDto: UpdateRecadoDto) {
+  update(
+    @Param('id', ParseIntPipe) id: string,
+    @Body() updateRecadoDto: UpdateRecadoDto,
+  ) {
     return this.RecadosService.update(+id, updateRecadoDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.RecadosService.remove(+id);
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.RecadosService.remove(id);
   }
 }
