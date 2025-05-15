ef5ad20d399020aba4ea37c865e0fbcfbf3e2567
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-15 20:30:25 -0300

    refactor: update update-recado DTO

--- crud-nest/src/recados/dto/update-recado.dto.ts
+++ crud-nest/src/recados/dto/update-recado.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateRecadoDto } from './create-recado.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateRecadoDto extends PartialType(CreateRecadoDto) {}
+export class UpdateRecadoDto extends PartialType(CreateRecadoDto) {
+  @IsBoolean()
+  @IsOptional()
+  readonly lido?: boolean;
+}
