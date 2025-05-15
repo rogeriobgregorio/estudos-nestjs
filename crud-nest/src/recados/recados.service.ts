52d2d97931f99d8c0820d797b36d2e9e95fedc40
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-14 23:45:29 -0300

    refactor: update recados service to use DTOs

--- crud-nest/src/recados/recados.service.ts
+++ crud-nest/src/recados/recados.service.ts
@@ -1,5 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { UpdateRecadoDto } from './dto/update-recado.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Recado } from './entites/recado.entity';
+import { CreateRecadoDto } from './dto/create-recado.dto';
 
 @Injectable()
 export class RecadosService {
@@ -20,37 +22,39 @@ export class RecadosService {
   }
 
   findOne(id: number): Recado {
-    const recado = this.recados.find((recado) => recado.id === id);
+    const recado = this.recados.find(recado => recado.id === id);
     if (!recado) {
-      throw new Error('Recado não encontrado');
+      throw new NotFoundException('Recado não encontrado');
     }
     return recado;
   }
 
-  create(recado: any) {
+  create(createRecadoDto: CreateRecadoDto): Recado {
     this.lastId++;
     const id = this.lastId;
     const novoRecado: Recado = {
       id,
-      ...recado,
+      ...createRecadoDto,
+      lido: false,
+      data: new Date(),
     };
     this.recados.push(novoRecado);
     return novoRecado;
   }
 
-  update(id: number, recado: Recado): Recado {
-    const index = this.recados.findIndex((recado) => recado.id === id);
+  update(id: number, updateRecadoDto: UpdateRecadoDto): Recado {
+    const index = this.recados.findIndex(recado => recado.id === id);
     if (index === -1) {
-      throw new Error('Recado não encontrado');
+      throw new NotFoundException('Recado não encontrado');
     }
-    this.recados[index] = { ...this.recados[index], ...recado };
+    this.recados[index] = { ...this.recados[index], ...updateRecadoDto };
     return this.recados[index];
   }
 
   remove(id: number): void {
-    const index = this.recados.findIndex((recado) => recado.id === id);
+    const index = this.recados.findIndex(recado => recado.id === id);
     if (index === -1) {
-      throw new Error('Recado não encontrado');
+      throw new NotFoundException('Recado não encontrado');
     }
     this.recados.splice(index, 1);
   }
