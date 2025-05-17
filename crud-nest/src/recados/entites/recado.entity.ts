4898cb548b32397de40beaf77b9a67e4f9530d7a
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-16 21:34:40 -0300

    refactor: update Recado entity structure

--- crud-nest/src/recados/entites/recado.entity.ts
+++ crud-nest/src/recados/entites/recado.entity.ts
@@ -1,8 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('recados')
 export class Recado {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column({ type: 'varchar', length: 255 })
   texto: string;
+
+  @Column({ type: 'varchar', length: 250 })
   de: string;
+
+  @Column({ type: 'varchar', length: 250 })
   para: string;
+
+  @Column({ default: false })
   lido: boolean;
+
+  @Column()
   data: Date;
+
+  @CreateDateColumn({ type: 'timestamp' })
+  createdAt?: Date;
+
+  @UpdateDateColumn({ type: 'timestamp' })
+  updatedAt?: Date;
 }
