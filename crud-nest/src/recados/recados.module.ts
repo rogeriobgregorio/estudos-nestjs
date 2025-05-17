36bd9523b344a95cad139e2a244e74ff8860539e
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-16 21:34:56 -0300

    chore: update Recados module configuration

--- crud-nest/src/recados/recados.module.ts
+++ crud-nest/src/recados/recados.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Recado } from './entites/recado.entity';
 import { RecadosController } from './recados.controller';
 import { RecadosService } from './recados.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Recado])],
   controllers: [RecadosController],
   providers: [RecadosService],
 })
