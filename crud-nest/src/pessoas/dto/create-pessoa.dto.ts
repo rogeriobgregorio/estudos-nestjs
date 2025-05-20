f52ee1a9ba13211338ff24b3b0e102d12b0c55d2
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-19 22:55:42 -0300

    refactor: update CreatePessoaDto with new validation or structure

--- crud-nest/src/pessoas/dto/create-pessoa.dto.ts
+++ crud-nest/src/pessoas/dto/create-pessoa.dto.ts
@@ -1,1 +1,24 @@
-export class CreatePessoaDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreatePessoaDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @MaxLength(100)
+  nome: string;
+}
