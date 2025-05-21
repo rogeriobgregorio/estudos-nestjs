a67199a8f47b39fa7dcd1b2d3b86dbf1e786db27
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-05-20 21:53:40 -0300

    refactor: improve Recado module configuration and service logic

--- crud-nest/src/recados/recados.module.ts
+++ crud-nest/src/recados/recados.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Recado } from './entites/recado.entity';
 import { RecadosController } from './recados.controller';
 import { RecadosService } from './recados.service';
+import { PessoasModule } from 'src/pessoas/pessoas.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Recado])],
+  imports: [TypeOrmModule.forFeature([Recado]), PessoasModule],
   controllers: [RecadosController],
   providers: [RecadosService],
 })
--- crud-nest/src/recados/recados.service.ts
+++ crud-nest/src/recados/recados.service.ts
@@ -4,22 +4,50 @@ import { Recado } from './entites/recado.entity';
 import { CreateRecadoDto } from './dto/create-recado.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { PessoasService } from '../pessoas/pessoas.service';
 
 @Injectable()
 export class RecadosService {
   constructor(
     @InjectRepository(Recado)
     private readonly recadoRepository: Repository<Recado>,
+    private readonly pessoasService: PessoasService,
   ) {}
 
   async findAll(): Promise<Recado[]> {
-    const recados = await this.recadoRepository.find();
+    const recados = await this.recadoRepository.find({
+      relations: ['de', 'para'],
+      order: {
+        id: 'DESC',
+      },
+      select: {
+        de: {
+          id: true,
+          nome: true,
+        },
+        para: {
+          id: true,
+          nome: true,
+        },
+      },
+    });
     return recados;
   }
 
   async findOne(id: number): Promise<Recado> {
     const recado = await this.recadoRepository.findOne({
       where: { id },
+      relations: ['de', 'para'],
+      select: {
+        de: {
+          id: true,
+          nome: true,
+        },
+        para: {
+          id: true,
+          nome: true,
+        },
+      },
     });
 
     if (!recado) {
@@ -28,15 +56,30 @@ export class RecadosService {
     return recado;
   }
 
-  async create(createRecadoDto: CreateRecadoDto): Promise<Recado> {
+  async create(createRecadoDto: CreateRecadoDto) {
+    const { deId, paraId } = createRecadoDto;
+    const de = await this.pessoasService.findOne(deId);
+    const para = await this.pessoasService.findOne(paraId);
+
     const novoRecado = {
-      ...createRecadoDto,
+      texto: createRecadoDto.texto,
+      de,
+      para,
       lido: false,
       data: new Date(),
     };
 
     const recado = this.recadoRepository.create(novoRecado);
-    return await this.recadoRepository.save(recado);
+    await this.recadoRepository.save(recado);
+    return {
+      ...recado,
+      de: {
+        id: recado.de.id,
+      },
+      para: {
+        id: recado.para.id,
+      },
+    };
   }
 
   async update(id: number, updateRecadoDto: UpdateRecadoDto): Promise<Recado> {
