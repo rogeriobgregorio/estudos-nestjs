276aaae4a86649ac4821124b95f286a785f4299f
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-06-08 12:00:00 -0300

    chore: update dependencies and adjust app module

--- crud-nest/src/app/app.module.ts
+++ crud-nest/src/app/app.module.ts
@@ -4,20 +4,24 @@ import { AppService } from './app.service';
 import { RecadosModule } from '../recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from '../pessoas/pessoas.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     RecadosModule,
     PessoasModule,
+    // Configuration module to load environment variables
+    ConfigModule.forRoot(),
+    // TypeORM configuration
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: '123456',
-      database: 'postgres',
-      autoLoadEntities: true,
-      synchronize: true, // Note: set to false in production
+      type: process.env.DATABASE_TYPE as 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+      synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Note: set to false in production
     }),
   ],
   controllers: [AppController],
