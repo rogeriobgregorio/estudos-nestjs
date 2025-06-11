c94ac7d3af6c3b715acf490b074e7f78b80fb8e8
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-06-11 12:00:00 -0300

    feat: add centralized app configuration

---
+++ crud-nest/src/app/app.config.ts
@@ -0,0 +1,13 @@
+export default () => ({
+  database: {
+    type: process.env.DATABASE_TYPE as 'postgres',
+    host: process.env.DATABASE_HOST,
+    port: Number(process.env.DATABASE_PORT),
+    username: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
+    synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE),
+  },
+  environment: process.env.NODE_ENV || 'development',
+});
--- crud-nest/src/app/app.module.ts
+++ crud-nest/src/app/app.module.ts
@@ -4,8 +4,9 @@ import { AppService } from './app.service';
 import { RecadosModule } from '../recados/recados.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PessoasModule } from '../pessoas/pessoas.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import Joi from '@hapi/joi';
+import appConfig from './app.config';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import Joi from '@hapi/joi';
     ConfigModule.forRoot({
       // envFilePath: ['.env', '.env.local'], // Load .env and .env.local files
       // ignoreEnvFile: false, // Do not ignore .env files
+      load: [appConfig], // Load custom configuration
       validationSchema: Joi.object({
         DATABASE_TYPE: Joi.string().required(),
         DATABASE_HOST: Joi.string().required(),
@@ -36,6 +38,25 @@ import Joi from '@hapi/joi';
       database: process.env.DATABASE_NAME,
       autoLoadEntities: Boolean(process.env.DATABASE_AUTOLOADENTITIES),
       synchronize: Boolean(process.env.DATABASE_SYNCHRONIZE), // Note: set to false in production
+    }),
+    // Alternatively, use TypeOrmModule.forRootAsync for dynamic configuration
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: configService.get<'postgres'>('database.type'),
+          host: configService.get<string>('database.host'),
+          port: configService.get<number>('database.port'),
+          username: configService.get<string>('database.username'),
+          database: configService.get<string>('database.database'),
+          password: configService.get<string>('database.password'),
+          autoLoadEntities: configService.get<boolean>(
+            'database.autoLoadEntities',
+          ),
+          synchronize: configService.get<boolean>('database.synchronize'),
+        };
+      },
     }),
   ],
   controllers: [AppController],
