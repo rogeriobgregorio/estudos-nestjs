0d386463793333e820169eee48a54be7e3c6758e
Rogério Bernardo Gregório <bernardo.rogerio93@gmail.com>
2025-07-02 12:00:00 -0300

    feat: update pessoas controller and add profile image asset

--- crud-nest/src/pessoas/pessoas.controller.ts
+++ crud-nest/src/pessoas/pessoas.controller.ts
@@ -7,6 +7,8 @@ import {
   Param,
   Delete,
   UseGuards,
+  UseInterceptors,
+  UploadedFile,
 } from '@nestjs/common';
 import { PessoasService } from './pessoas.service';
 import { CreatePessoaDto } from './dto/create-pessoa.dto';
@@ -15,6 +17,8 @@ import { RecadosUtils } from 'src/recados/recados.utils';
 import { AuthTokenGuard } from 'src/auth/guards/auth-token.guard';
 import { TokenPayloadParam } from 'src/auth/params/token-payload.param';
 import { TokenPayloadDto } from 'src/auth/dto/token-payload.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { buffer } from 'stream/consumers';
 
 @Controller('pessoas')
 export class PessoasController {
@@ -57,5 +61,21 @@ export class PessoasController {
     @TokenPayloadParam() tokenPayload: TokenPayloadDto,
   ) {
     return this.pessoasService.remove(+id, tokenPayload);
+  }
+
+  @UseGuards(AuthTokenGuard)
+  @UseInterceptors(FileInterceptor('file'))
+  @Post('upload-picture')
+  uploadPicture(
+    @UploadedFile() file: Express.Multer.File,
+    @TokenPayloadParam() tokenPayload: TokenPayloadDto,
+  ) {
+    return {
+      fieldName: file.fieldname,
+      originalName: file.originalname,
+      mimeType: file.mimetype,
+      buffer: {},
+      size: file.size,
+    };
   }
 }
